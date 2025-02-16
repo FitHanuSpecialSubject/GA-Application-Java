523caa9332a0e68c437f7fbdf99a7cf2a566322f
thanggw <157234766+thanggw@users.noreply.github.com>
2025-02-16 22:05:09 +0700

    dynamic validation (#19)
    * dynamic validation
    
    * dynamic validation 2
    
    * dynamic validator 3
    
    ---------
    
    Co-authored-by: Vũ Nhật Hoàng <110092300+han8129@users.noreply.github.com>

---
+++ src/main/java/org/fit/ssapp/dto/validator/StableMatchingValidator.java
@@ -0,0 +1,78 @@
+package org.fit.ssapp.dto.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.fit.ssapp.config.ValidationConfig;
+import org.fit.ssapp.constants.MessageConst;
+import org.fit.ssapp.dto.request.StableMatchingProblemDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StableMatchingValidator implements ConstraintValidator<ValidStableMatching, StableMatchingProblemDto> {
+
+    private final ValidationConfig validationConfig;
+
+    @Autowired
+    public StableMatchingValidator(ValidationConfig validationConfig) {
+        this.validationConfig = validationConfig;
+    }
+
+    @Override
+    public boolean isValid(StableMatchingProblemDto dto, ConstraintValidatorContext context) {
+        boolean isValid = true;
+
+        if (dto.getPopulationSize() > validationConfig.getMaxPopulation()) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Population size must not exceed " + validationConfig.getMaxPopulation())
+                    .addPropertyNode("populationSize")
+                    .addConstraintViolation();
+            isValid = false;
+        }
+
+        if (dto.getGeneration() > validationConfig.getMaxGeneration()) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Generation must not exceed " + validationConfig.getMaxGeneration())
+                    .addPropertyNode("generation")
+                    .addConstraintViolation();
+            isValid = false;
+        }
+
+        if (dto.getNumberOfIndividuals() < validationConfig.getMinIndividuals()) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Number of individuals must be at least " + validationConfig.getMinIndividuals())
+                    .addPropertyNode("numberOfIndividuals")
+                    .addConstraintViolation();
+            isValid = false;
+        }
+
+        if (dto.getPopulationSize() < 1) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(MessageConst.ErrMessage.POPULATION_SIZE)
+                    .addPropertyNode("populationSize")
+                    .addConstraintViolation();
+            isValid = false;
+        }
+
+        if (dto.getGeneration() < 1) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(MessageConst.ErrMessage.GENERATION)
+                    .addPropertyNode("generation")
+                    .addConstraintViolation();
+            isValid = false;
+        }
+
+        if (dto.getNumberOfIndividuals() < 3) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(MessageConst.ErrMessage.MES_002)
+                    .addPropertyNode("numberOfIndividuals")
+                    .addConstraintViolation();
+            isValid = false;
+        }
+
+        return isValid;
+    }
+}
+
+
+
