420603c3fb0bd02e84ddd45eeb0993bb246cdddd
dthuy0211 <damt9362@gmail.com>
2025-04-11 03:42:55 +0700

    xóa case testEmptyRequestBody()
    vì springboot tự xử lí dc trường hợp này

--- src/test/java/org/fit/ssapp/service/gt/GTIntegrationTest.java
+++ src/test/java/org/fit/ssapp/service/gt/GTIntegrationTest.java
@@ -83,9 +83,8 @@ public class GTIntegrationTest {
     GameTheoryProblemDto dto = setUpBaseCase(algorithm);
 
     List<Conflict> conflicts = Arrays.asList(
-            createConflict(0,1,0,1),
-            createConflict(1,2,1,0)
-
+            createConflict(1,2,0,1),
+            createConflict(2,3,1,0)
     );
     dto.setConflictSet(conflicts);
 
@@ -105,46 +104,34 @@ public class GTIntegrationTest {
   }
 
   /**
-   * Test case for base case with empty request .
-   *
-   */
-  @Test
-  void testEmptyRequestBody() throws Exception{
-
-    MvcResult result = mockMvc
-            .perform(post("/api/game-theory-solver")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content("{}")) // Empty JSON body
-            .andDo(print())
-            .andExpect(status().isBadRequest())
-            .andReturn();
-
-    String response = getAsyncResponse(result);
-
-  }
-
-  /**
    * Test case for base case with invalid fitness function .
    *
    */
   @Test
   void testInvalidFitnessFunc() throws Exception{
-
     GameTheoryProblemDto dto = setUpBaseCase("NSGAII");
     dto.setFitnessFunction("Wrong fitness function");
 
-    MvcResult result = performPostRequest(dto);
+    MvcResult result = mockMvc
+            .perform(post("/api/game-theory-solver")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(request().asyncStarted())
+            .andReturn();
 
-    String response = getAsyncResponse(result);
+    String response = mockMvc.perform(asyncDispatch(result))
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
 
     JsonNode jsonNode = objectMapper.readTree(response);
-    assertThat(jsonNode.has("data")).isTrue();
-    JsonNode dataNode = jsonNode.get("data");
-
     assertThat(jsonNode.has("message")).isTrue();
     String errorMessage = jsonNode.get("message").asText();
-    assertThat(errorMessage).contains("Unknown function or variable");
+    assertThat(errorMessage).contains("Invalid fitness function syntax");
   }
+
 
   private MvcResult performPostRequest(GameTheoryProblemDto dto) throws Exception {
     return mockMvc
@@ -163,6 +150,7 @@ public class GTIntegrationTest {
             .getResponse()
             .getContentAsString();
   }
+
 
   /**
    * Set up input data for base case .
@@ -257,8 +245,6 @@ public class GTIntegrationTest {
               && player2Node.get("strategyName").asText().equals("Strategy " + conflict.getRightPlayerStrategy())
       ).isFalse();
     }
-
-
   }
 
   /**
